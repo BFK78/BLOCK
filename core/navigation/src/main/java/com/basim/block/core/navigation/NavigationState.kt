37a3c8bea83266d67e9af0d7a779acc795cf6a0a
AnoonaNasrin <your-anoonanasrinp24@gmail.com>
2026-07-09 11:01:13 +0530

    Add navigation state

--- core/navigation/src/main/java/com/basim/block/core/navigation/NavigationState.kt
+++ core/navigation/src/main/java/com/basim/block/core/navigation/NavigationState.kt
@@ -1,2 +1,44 @@
 package com.basim.block.core.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberNavBackStack
+
+@Composable
+fun rememberNavigationState(
+    startKey: NavKey,
+    topLevelKeys: Set<NavKey>
+): NavigationState {
+    val topLevelStack = rememberNavBackStack(startKey)
+    val subStacks = topLevelKeys.associateWith { key -> rememberNavBackStack(key) }
+    return remember(startKey, topLevelKeys) {
+        NavigationState(
+            startKey = startKey,
+            topLevelStack = topLevelStack,
+            subStacks = subStacks
+        )
+    }
+}
+
+class NavigationState(
+    val startKey: NavKey,
+    val topLevelStack: NavBackStack<NavKey>,
+    val subStacks: Map<NavKey, NavBackStack<NavKey>>
+) {
+
+    val currentTopLevelKey: NavKey by derivedStateOf { topLevelStack.last() }
+
+    val topLevelKeys
+        get() = subStacks.keys
+
+    val currentSubStack: NavBackStack<NavKey>
+        get() = subStacks[currentTopLevelKey]
+            ?: error("substack for $currentTopLevelKey dose not exist")
+
+    val currentKey: NavKey by derivedStateOf { currentSubStack.last() }
+}
